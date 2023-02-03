be4e87949ea65b2c565c8531d056fa7ebde10bde
stijndcl <declercq.stijn@outlook.com>
2023-02-03 17:20:50 +0100

    Init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		color: theme(colors.white);
+		background-color: theme(colors.gray.900);
+	}
+</style>
