5f8168c25fd52c542a7e69e6ecefb0fe576cd1ae
Stijn De Clercq <declercq.stijn@outlook.com>
2023-02-19 00:34:04 +0100

    Fix warning

--- src/lib/components/footer/FooterIcon.svelte
+++ src/lib/components/footer/FooterIcon.svelte
@@ -8,6 +8,7 @@
 	class="opacity-100 mx-3 flex flex-row supportshover:hover:cursor-pointer text-gray-600 supportshover:hover:text-white fill-gray-600 supportshover:hover:fill-white motion-safe:transition-colors motion-safe:ease-in-out motion-safe:duration-200"
 	href={url}
 	target="_blank"
+	rel="noreferrer"
 >
 	<div class="w-5 h-5">
 		<slot />
--- src/lib/components/projects/ProjectsSection.svelte
+++ src/lib/components/projects/ProjectsSection.svelte
@@ -2,4 +2,4 @@
 	import PortfolioSection from "$lib/components/common/PortfolioSection.svelte";
 </script>
 
-<PortfolioSection title="Projects">VSC Plugin</PortfolioSection>
+<PortfolioSection title="Projects">VSC Plugin, ugent-food</PortfolioSection>
