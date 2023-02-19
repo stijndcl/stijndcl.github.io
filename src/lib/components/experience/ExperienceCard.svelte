a3402e757019ccf75d5dc525a67f8ac6f572665e
Stijn De Clercq <declercq.stijn@outlook.com>
2023-02-19 01:50:30 +0100

    Drop shadow

--- src/lib/components/experience/ExperienceCard.svelte
+++ src/lib/components/experience/ExperienceCard.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <article
-	class="p-6 bg-gray-900 rounded-lg border border-gray-700 shadow-md text-start supportshover:opacity-40 supportshover:hover:opacity-100 supportshover:hover:border-cyan-400 motion-safe:transition-colors motion-safe:transition-opacity motion-safe:duration-300 motion-safe:ease-in-out"
+	class="p-6 bg-gray-900 rounded-lg border border-gray-700 shadow-lg shadow-black text-start supportshover:opacity-40 supportshover:hover:opacity-100 supportshover:hover:border-cyan-400 motion-safe:transition-colors motion-safe:transition-opacity motion-safe:duration-300 motion-safe:ease-in-out"
 >
 	<div class="flex justify-between items-center mb-5 text-gray-500">
 		<span class="font-bold text-xl text-white mr-5">
