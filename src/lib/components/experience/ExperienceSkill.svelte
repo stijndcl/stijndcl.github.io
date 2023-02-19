774f2456e093b7a896cc477d7612ad31f192ff6a
Stijn De Clercq <declercq.stijn@outlook.com>
2023-02-19 14:14:40 +0100

    Make skill icons grayscale

--- src/lib/components/experience/ExperienceSkill.svelte
+++ src/lib/components/experience/ExperienceSkill.svelte
@@ -1,4 +1,4 @@
 <!-- TODO tooltip to reveal the name when hovering over -->
-<span class="w-6 h-6">
+<span class="w-6 h-6 supportshover:grayscale supportshover:group-hover:grayscale-0">
 	<slot />
 </span>
