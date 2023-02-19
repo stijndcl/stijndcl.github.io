5fcf64559b50c1fcbdceb18e613cd7624acadfa1
Stijn De Clercq <declercq.stijn@outlook.com>
2023-02-20 00:05:52 +0100

    Typora

--- src/lib/components/skills/Software.svelte
+++ src/lib/components/skills/Software.svelte
@@ -6,9 +6,14 @@
 	import IntelliJIcon from "../../../assets/icons/intellij.png";
 	import GoLandIcon from "../../../assets/icons/goland.png";
 	import CLionIcon from "../../../assets/icons/clion.png";
+	import TyporaIcon from "../../../assets/icons/typora.png";
+	import PostmanIcon from "../../../assets/icons/postman.svg?component";
+	import InkscapeIcon from "../../../assets/icons/inkscape.png";
 	import VSCodeIcon from "../../../assets/icons/vscode.png";
 </script>
 
+<!-- Some icons here are .png's instead of .svg's, because of
+ 		a bug with linear gradients in the svg plugin -->
 <SkillSubsection label="Software">
 	<SkillsGrid>
 		<SkillCard name="PyCharm">
@@ -22,6 +27,16 @@
 		</SkillCard>
 		<SkillCard name="CLion">
 			<img src={CLionIcon} alt="CLion" />
+		</SkillCard>
+		<SkillCard name="Typora">
+			<img src={TyporaIcon} alt="Typora" />
+		</SkillCard>
+		<SkillCard name="Postman">
+			<PostmanIcon />
+		</SkillCard>
+		<SkillCard name="Inkscape">
+			<!--	This is very ironically not an SVG		-->
+			<img src={InkscapeIcon} alt="Inkscape" />
 		</SkillCard>
 		<SkillCard name="Visual Studio Code">
 			<img src={VSCodeIcon} alt="Visual Studio Code" />
