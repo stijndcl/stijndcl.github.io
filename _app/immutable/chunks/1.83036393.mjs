6add91f2f85097b95fe10fc73c2abe22ef8dc54a
stijndcl <stijndcl@users.noreply.github.com>
2023-02-25 16:03:12 +0000

    Hide projects button from hero bcb4300398270592668d3461bc069e9308fe9a28

--- _app/immutable/chunks/1.83036393.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.aea71af9.mjs";export{t as component};
--- _app/immutable/chunks/2.b87f81c3.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.fe0fb1b9.mjs";export{t as component};
