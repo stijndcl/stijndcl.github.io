d9ee0dadcbc19bb84c6563f7761ff316c90f3f7f
stijndcl <stijndcl@users.noreply.github.com>
2023-02-26 00:55:41 +0000

    Add background color to footer 04d5bfa232fcb0f3e143b4f1626dd159ea6baac1

--- _app/immutable/chunks/1.7a2d8911.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.4ddeadfa.mjs";export{t as component};
--- _app/immutable/chunks/2.6e46e90a.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.18df4a22.mjs";export{t as component};
