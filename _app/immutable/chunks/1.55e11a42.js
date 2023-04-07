d4ea6e69b5f2d8486d921af7851f1d027a5ebf9e
stijndcl <stijndcl@users.noreply.github.com>
2023-04-07 19:58:45 +0000

    Merge pull request #19 from stijndcl/dependabot/npm_and_yarn/sveltejs/kit-1.15.2
    Bump @sveltejs/kit from 1.15.1 to 1.15.2 20ead2be2c70fcf7d23f57840c1cb310cb7b7f6f

---
+++ _app/immutable/chunks/1.55e11a42.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.8199a5fd.js";export{t as component};
--- _app/immutable/chunks/1.878756f0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.ed04a8a4.js";export{t as component};
