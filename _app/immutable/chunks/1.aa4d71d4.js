8c54ed9fbdde4bb80a66c5722f9d94af7309b848
stijndcl <stijndcl@users.noreply.github.com>
2023-04-08 15:43:06 +0000

    Merge pull request #3 from stijndcl/dependabot/npm_and_yarn/sveltejs/adapter-auto-2.0.0
    Bump @sveltejs/adapter-auto from 1.0.3 to 2.0.0 94afc0d9cccc618bb5a91b7d2e5a59fd3c7d4adc

---
+++ _app/immutable/chunks/1.aa4d71d4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.29bd61c9.js";export{t as component};
--- _app/immutable/chunks/1.e51d5b52.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.63c210b7.js";export{t as component};
